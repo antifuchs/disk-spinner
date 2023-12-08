bab5cfeddf7b13dc7955986d3dc5cbf809793cbb
Andreas Fuchs <asf@boinkor.net>
2023-12-08 14:13:57 -0500

    Fill the given device with non-random data
    This actually kinda works now! Just needs a few more bits:
    
    * Exit out of the loop when we've reached the end
    * Generate random data
    * Hash that data.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+#[macro_use]
+extern crate lazy_static;
+
+use indicatif::ProgressStyle;
+use tracing::Span;
+use tracing_indicatif::span_ext::IndicatifSpanExt;
+use tracing_indicatif::IndicatifLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+use anyhow::Context;
 use clap::Parser;
-use std::{path::PathBuf, str::FromStr};
-use tracing::{info, warn};
+use std::{
+    fs::OpenOptions,
+    io::Write,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+use tracing::{info, info_span, warn};
 
 #[derive(Debug, Clone)]
 struct ValidDevice {
@@ -34,6 +50,12 @@ struct Args {
     #[clap(value_parser = clap::value_parser!(ValidDevice))]
     device: ValidDevice,
 
+    /// Number of bytes to buffer for writing.
+    ///
+    /// Defaults to the physical block size of the device (or 8192 if that is unset).
+    #[clap(long)]
+    buffer_size: Option<usize>,
+
     /// Test the device even if the media type is not a spinning disk.
     #[clap(long)]
     allow_any_media: bool,
@@ -45,8 +67,11 @@ struct Args {
 }
 
 fn main() -> anyhow::Result<()> {
-    tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::new())
-        .expect("setting tracing default failed");
+    let indicatif_layer = IndicatifLayer::new();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().with_writer(indicatif_layer.get_stderr_writer()))
+        .with(indicatif_layer)
+        .init();
     let args = Args::parse();
 
     let ValidDevice {
@@ -64,11 +89,63 @@ fn main() -> anyhow::Result<()> {
     }
     if device.media_type != block_utils::MediaType::Rotational {
         if !args.allow_any_media {
-            anyhow::bail!("Device is not a rotational disk - this tool may be harmful for solid-state drives! Pass --allow-any-media to run anyway.");
+            anyhow::bail!("Device is not a rotational disk - this tool may be harmful to solid-state drives and others! Pass --allow-any-media to run anyway.");
         } else {
             warn!(?device.media_type, "Media type is not as expected but running tests anyway.");
         }
     }
-    info!(?partition, ?device, ?path);
+    // TODO: Maybe test that the disk is empty?
+
+    info!(?partition, ?device, ?path, "Starting test");
+
+    write_test(
+        &path,
+        &device,
+        args.buffer_size.unwrap_or_else(|| {
+            device
+                .physical_block_size
+                .unwrap_or(8192)
+                .try_into()
+                .unwrap()
+        }),
+    )
+    .context("Write test")?;
+    Ok(())
+}
+
+lazy_static! {
+    static ref PROGRESS_STYLE: ProgressStyle = ProgressStyle::with_template(
+        "[{elapsed_precise}] {bar:40.white/grey} {bytes}/{total_bytes} ({bytes_per_sec}, ETA {eta_precise}) {msg}",
+    ).expect("Internal error in indicatif progress bar template syntax");
+}
+
+// TODO: return hashed data
+#[tracing::instrument(skip(dev, buffer_size))]
+fn write_test(
+    dev_path: &Path,
+    dev: &block_utils::Device,
+    buffer_size: usize,
+) -> anyhow::Result<()> {
+    let mut out = OpenOptions::new()
+        .write(true)
+        .open(dev_path)
+        .with_context(|| format!("Opening the device {:?} for writing", dev_path))?;
+
+    let bar_span = info_span!("writing");
+    bar_span.pb_set_style(&PROGRESS_STYLE);
+    bar_span.pb_set_length(dev.capacity);
+    let bar_span_handle = bar_span.enter();
+
+    let mut buf: Vec<u8> = Vec::with_capacity(buffer_size);
+    loop {
+        for _n in 0..200 {
+            buf.write_all(&[0xca, 0xfe, 0xba, 0xbe])
+                .context("filling buffer")?;
+        }
+        out.write_all(&buf)?; // feca beba for some reason
+        Span::current().pb_inc(buf.len().try_into().unwrap());
+        buf.clear();
+    }
+
     Ok(())
 }
