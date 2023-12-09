805ffb31404f3cfac13b84788bedf0b20989acae
Andreas Fuchs <asf@boinkor.net>
2023-12-09 15:57:26 -0500

    Fix warning on linux

--- src/linux.rs
+++ src/linux.rs
@@ -39,14 +39,18 @@ pub(crate) fn sanity_checks(
         if !args.allow_any_block_device {
             anyhow::bail!("Device is not a whole disk but a partition - pass --allow-any-block-device to run tests anyway.");
         } else {
-            warn!(?partition, "Testing a partition but running tests anyway.");
+            warn!(
+                ?partition,
+                ?device_path,
+                "Testing a partition but running tests anyway."
+            );
         }
     }
     if device.media_type != block_utils::MediaType::Rotational {
         if !args.allow_any_media {
             anyhow::bail!("Device is not a rotational disk - this tool may be harmful to solid-state drives and others! Pass --allow-any-media to run anyway.");
         } else {
-            warn!(?device.media_type, "Media type is not as expected but running tests anyway.");
+            warn!(?device.media_type, ?device_path, "Media type is not as expected but running tests anyway.");
         }
     }
     // TODO: Maybe test that the disk is empty?
