4af4243e7ddc820fe7887ce5d2993f0778b0ea6f
Andreas Fuchs <asf@boinkor.net>
2025-04-01 10:31:09 -0400

    Try to notice stalls on either side of the channel

--- src/write_test.rs
+++ src/write_test.rs
@@ -4,6 +4,7 @@ use crate::crypto::GarbageGenerator;
 use crate::metadata::TestOptions;
 use crate::PROGRESS_STYLE;
 use anyhow::Context as _;
+use async_channel::{TryRecvError, TrySendError};
 use compio::fs::OpenOptions;
 use compio::io::AsyncWriteExt;
 use compio::runtime::spawn;
@@ -12,15 +13,13 @@ use std::{
     io::{self},
     path::Path,
 };
-use tracing::{info_span, warn};
+use tracing::{info, warn, Span};
 use tracing_indicatif::span_ext::IndicatifSpanExt;
 
-#[tracing::instrument(skip(opts))]
+#[tracing::instrument(name = "write test", skip(opts))]
 pub(crate) async fn write(dev_path: &Path, opts: &TestOptions) -> anyhow::Result<()> {
-    let bar_span = info_span!("writing");
-    bar_span.pb_set_style(&PROGRESS_STYLE);
-    bar_span.pb_set_length(opts.device_capacity);
-    let _current_span = bar_span.enter();
+    Span::current().pb_set_style(&PROGRESS_STYLE);
+    Span::current().pb_set_length(opts.device_capacity);
 
     let mut out = Cursor::new(
         OpenOptions::new()
@@ -41,18 +40,35 @@ pub(crate) async fn write(dev_path: &Path, opts: &TestOptions) -> anyhow::Result
                     warn!(%error, "Could not fill buffer with random-ish bytes");
                     return;
                 }
-                if let Err(error) = bytes_send.send(buf).await {
-                    warn!(%error, "Could not send random bytes across to consumer");
-                    return;
-                };
+                match bytes_send.try_send(buf) {
+                    Ok(()) => {}
+                    Err(TrySendError::Full(buf)) => {
+                        info!("Byte generator pipeline stalled; blocking...");
+                        if let Err(error) = bytes_send.send(buf).await {
+                            warn!(%error, "Could not send random bytes across to consumer");
+                            return;
+                        };
+                    }
+                    Err(TrySendError::Closed(_)) => {
+                        warn!("Could not send random bytes across to consumer; channel is closed");
+                        return;
+                    }
+                }
             }
         }
     });
     loop {
-        let mut buf = bytes_recv
-            .recv()
-            .await
-            .context("Receiving random-ish bytes")?;
+        let mut buf = match bytes_recv.try_recv() {
+            Ok(buf) => buf,
+            Err(TryRecvError::Empty) => {
+                info!("receiving bytes to write: pipeline stall");
+                bytes_recv
+                    .recv()
+                    .await
+                    .context("Could not receive bytes to write after stall")?
+            }
+            e => e.context("Could not receive bytes to write")?,
+        };
         let res;
         (res, buf) = out.write_all(buf).await.into();
         match res {
@@ -66,6 +82,6 @@ pub(crate) async fn write(dev_path: &Path, opts: &TestOptions) -> anyhow::Result
             }
             Err(e) => anyhow::bail!("io Error {:?}: kind {:?}", e, e.kind()),
         }
-        bar_span.pb_inc(buf.len() as u64);
+        Span::current().pb_inc(buf.len() as u64);
     }
 }
